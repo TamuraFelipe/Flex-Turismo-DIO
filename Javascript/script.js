e8a11215033d1cff7892942cb36bf1c732df8b12
Tamura <tamurafelipe@gmail.com>
2022-06-13 18:53:22 -0300

    Flex Turismo DIO
    Projeto com fundamentos ao Flexbox

---
+++ Javascript/script.js
@@ -0,0 +1,11 @@
+ScrollReveal({
+    origin: 'top',
+    distance: '30px',
+    duration: 700
+  }).reveal(`
+    #header-container,
+    #first-container,
+    #about,
+    #services,
+    #plans`)
+  
